50780c846fb1b6d326a94cdd5fb2767dd3d8dde4
DanTheGeekMan <danthegeekman@googlemail.com>
2016-08-21 16:52:51 +0100

    Hello world in jQuery and adding buttons to variable.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,16 @@
 var deliveryRescue = deliveryRescue || {}
 
-deliveryRescue.getButtons = function() {
-  this.buttons = document.querySelectorAll(".buttons");
-  console.log("Buttons " + this.buttons + ".");
-}
-deliveryRescue.addListeners = function(addListenerTo) {
-  for (var i = 0; i < addListenerTo.length; i++) {
-    addListenerTo[i].addEventListener("click", function(){
-this.test = event.target.getAttribute('id');
-      console.log("hello world " + this.test + ".");});
-  }
-}
-
-
+// deliveryRescue.getButtons = function() {
+//   this.buttons = document.querySelectorAll(".buttons");
+//   console.log("Buttons " + this.buttons + ".");
+// }
+// deliveryRescue.addListeners = function(addListenerTo) {
+//   for (var i = 0; i < addListenerTo.length; i++) {
+//     addListenerTo[i].addEventListener("click", function(){
+// this.test = event.target.getAttribute('id');
+//       console.log("hello world " + this.test + ".");});
+//   }
+// }
 
 
 
@@ -27,8 +25,12 @@ this.test = event.target.getAttribute('id');
 
 
 deliveryRescue.setup = function() {
-  this.getButtons();
-  this.addListeners(this.buttons);
+  $.buttonsVar = $('.buttons')
+console.log("buttons are " + $.buttonsVar);
+  // this.getButtons();
+  // this.addListeners(this.buttons);
 }
 
-window.addEventListener("DOMContentLoaded", deliveryRescue.setup.bind(deliveryRescue));
+$(document).ready(deliveryRescue.setup);
+
+// window.addEventListener("DOMContentLoaded", deliveryRescue.setup.bind(deliveryRescue));
