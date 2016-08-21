e4f6069a407c5cf7c70b012bad520643f1f5a85e
DanTheGeekMan <danthegeekman@googlemail.com>
2016-08-21 16:28:13 +0100

    Added 3 event listeners.  Tested and working.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-console.log("working")
+var deliveryRescue = deliveryRescue || {}
+
+deliveryRescue.getButtons = function() {
+  this.buttons = document.querySelectorAll(".buttons");
+  console.log("Buttons " + this.buttons + ".");
+}
+deliveryRescue.addListeners = function(addListenerTo) {
+  for (var i = 0; i < addListenerTo.length; i++) {
+    addListenerTo[i].addEventListener("click", function(){
+this.test = event.target.getAttribute('id');
+      console.log("hello world " + this.test + ".");});
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+deliveryRescue.setup = function() {
+  this.getButtons();
+  this.addListeners(this.buttons);
+}
+
+window.addEventListener("DOMContentLoaded", deliveryRescue.setup.bind(deliveryRescue));
