55e3d16e3dea6fafbaecb9b989c46bdfaed7c588
DanTheGeekMan <danthegeekman@googlemail.com>
2016-08-21 18:50:31 +0100

    Added buttonId as passed parameter.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,43 @@
 var deliveryRescue = deliveryRescue || {}
 
+deliveryRescue.moveTruck = function(){
+  console.log("ID " + $(this).attr("Id"))
+  if ($(this).attr("id") === "right_button") {
+    deliveryRescue.moveDirection = -10;
+    console.log("Move right");
+  } else {
+    deliveryRescue.moveDirection = 10;
+    console.log("Move left");
+  }
+}
+
+deliveryRescue.resetGame = function(buttonId){
+  console.log("Reset function called.")
+  console.log("Clicked buttons is " + buttonId);
+}
+
 deliveryRescue.clickButtons = function() {
-  console.log("Clicked buttons is " + $(this).attr("id"));
+  if ($(this).attr("id") === "reset_button") {
+    console.log("reset event check 1" + $(this).attr("id"))
+    deliveryRescue.resetGame($(this).attr("id"));
+    console.log("reset event check last" + $(this).attr("id"))
+      } else {
+      deliveryRescue.moveTruck();
+  }
 }
 
 deliveryRescue.addListeners = function() {
   $('.buttons').on("click", deliveryRescue.clickButtons);
+  // $(function(){
+  //   $('div').on("click", function() {
+  //     var $element = $(this);
+  //     $element.hide("slow", "swing", function(){
+  //       setTimeout(function(){
+  //         $element.show();
+  //       }, 1000);
+  //     });
+  //   });
+  // });
 }
 
 deliveryRescue.setup = function() {
