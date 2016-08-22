99163443ee66a5f3e9ae7894ebeb81771159def1
DanTheGeekMan <danthegeekman@googlemail.com>
2016-08-22 06:23:20 +0100

    Notify box working correctly.  Timer broken.

--- js/app.js
+++ js/app.js
@@ -1,24 +1,39 @@
 var deliveryRescue = deliveryRescue || {}
+//var $timer = $('#timer');
+//var $startStop = $timer.find('#startStop');
+//var $reset = $timer.find('#reset');
+//var $screen = $timer.find('#screen');
+var ms = 0;
+var s = 0;
+var timer;
 
 deliveryRescue.moveTruck = function(direction){
   deliveryRescue.truckPosition = parseInt($('#truck').css("left").replace("px", ""))
   if (direction === "left_button") {
     deliveryRescue.truckPosition += -10;
-    } else {
+  } else {
     deliveryRescue.truckPosition += 10; }
-  $('#truck').css("left", deliveryRescue.truckPosition);
-}
-
-deliveryRescue.resetGame = function(){
-  $('#truck').css("left", 290);
-
+    $('#truck').css("left", deliveryRescue.truckPosition);
+    console.log("Timer " + s)
   }
 
-deliveryRescue.clickButtons = function() {
-  if ($(this).attr("id") === "reset_button") {
-    deliveryRescue.resetGame();
-      } else {
+  deliveryRescue.resetGame = function(){
+    $('#truck').css("left", 290);
+  }
+
+  deliveryRescue.clickButtons = function() {
+    if ($("#notifyBox").css("display") === "block") {
+      if ($(this).attr('id') === "clearNotifyBox") {
+       $('#clearNotifyBox').css("display", "none");
+       $('#notifyBox').css("display", "none");
+       deliveryRescue.timer();     
+     }
+   } else {
+     if ($(this).attr("id") === "reset_button") {
+      deliveryRescue.resetGame();
+    } else {
       deliveryRescue.moveTruck($(this).attr("id"));
+    } 
   }
 }
 
@@ -36,8 +51,39 @@ deliveryRescue.addListeners = function() {
   // });
 }
 
+deliveryRescue.timer = function() {
+  function padNum(num) {
+    return num < 10 ? "0" + num : String(num);
+  }
+  //$startStop.on("click", function() {
+    if(!$timer.hasClass('running')) {
+      $timer.addClass('running');
+      timer = setInterval(function() {
+        ms++;
+        if(ms > 99) {
+          ms = 0;
+          s++;
+        }
+        $screen.text(padNum(s));
+      }, 10);
+    } else {
+      $timer.removeClass('running');
+      clearTimeout(timer);
+    }
+  //});
+  $reset.on('click', function() {
+    $timer.removeClass('running');
+    clearTimeout(timer);
+    $screen.text("00");
+    ms = 0;
+    s = 0;
+  });
+};
+
+
 deliveryRescue.setup = function() {
   deliveryRescue.addListeners();
 }
+
 
 $(document).ready(deliveryRescue.setup.bind(deliveryRescue));
