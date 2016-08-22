49ec53ccbc9af96fe6e7def8eaf6d425c800324f
DanTheGeekMan <danthegeekman@googlemail.com>
2016-08-22 14:53:36 +0100

    Before I mess everything up.

--- js/app.js
+++ js/app.js
@@ -18,33 +18,49 @@ deliveryRescue.moveTruck = function(direction){
   }
 
   deliveryRescue.boxFall = function () {
+    deliveryRescue.boxCount = 10;
     var $boxObj = $('#box');
     var top = 360;
-    var left = 200;
-    $boxObj.css({
-      left: left,
-      top: 0,
-      opacity: 0,
-      display: 'block'
-    }).animate({
-      left: left,
-      top: 310,
-      opacity: 1
-    }, 2500, 'easeOutBounce');
+    var left = 200; for(i = 0; i < deliveryRescue.boxCount; i++) {
+      $boxObj.css({
+        left: left,
+        top: 0,
+        opacity: 0,
+        display: 'block'
+      }).animate({
+        left: left,
+        top: 310,
+        opacity: 1
+      }, 2500, 'easeOutBounce');
+    }
   }
 
-  deliveryRescue.timerFunction = function() {
-    setInterval(function() { deliveryRescue.timerCounter++;
-    }, 1000);
-  }
+//   deliveryRescue.collisionDetection = function () {
+//     var rect1 = {x: 5, y: 5, width: 50, height: 50}
+//     var rect2 = {x: 20, y: 10, width: 10, height: 10}
 
-  deliveryRescue.clickButtons = function() {
-    console.log("Button clicked " + $(this).attr('id') + " and the timer is at " + deliveryRescue.timerCounter + " seconds.");
-    if ($("#notifyBox").css("display") === "block") {
-      if ($(this).attr('id') === "clearNotifyBox") {
-       $('#clearNotifyBox').css("display", "none");
-       $('#notifyBox').css("display", "none");
-       deliveryRescue.timerFunction();   
+//     if (rect1.x < rect2.x + rect2.width &&
+//      rect1.x + rect1.width > rect2.x &&
+//      rect1.y < rect2.y + rect2.height &&
+//      rect1.height + rect1.y > rect2.y) {
+//       // collision detected!
+//   }
+// }
+
+deliveryRescue.timerFunction = function() {
+  setInterval(function() { deliveryRescue.timerCounter++;
+  }, 1000);
+}
+
+deliveryRescue.clickButtons = function() {
+  console.log("Button clicked " + $(this).attr('id') + " and the timer is at " + deliveryRescue.timerCounter + " seconds.");
+  if ($("#notifyBox").css("display") === "block") {
+    if ($(this).attr('id') === "clearNotifyBox") {
+     $('#clearNotifyBox').css("display", "none");
+     $('#notifyBox').css("display", "none");
+     deliveryRescue.timerFunction();   
+       // deliveryRescue.collisionDetection();
+       deliveryRescue.boxFall();  
        deliveryRescue.boxFall();  
      }
    } else {
